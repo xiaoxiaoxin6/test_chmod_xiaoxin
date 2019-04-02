0c33673ede95d895ed2ec95e3a1a726aadcc84a2
xiaoxiaoxin6 <xiaoxiaoxin6@163.com>
2019-04-02 18:58:26 +0800

    1st commit

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="test_chmod_xiaoxin",
+    version="0.0.2",
+    author="xiaoxin",
+    author_email="xiaoxiaoxin6@163.com",
+    description="test the right setting of the files",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/xiaoxiaoxin6/test_chmod_xiaoxin.git",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
